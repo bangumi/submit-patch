6564cb3098e169d46748095b0bb219b47ab545c9
Trim21 <trim21.me@gmail.com>
2024-08-22 14:41:09 +0800

    minor refactor

--- server/contrib.py
+++ server/contrib.py
@@ -124,7 +124,6 @@ async def suggest_api(
         insert into patch (id, subject_id, from_user_id, description, name, infobox, summary, nsfw,
                            original_name, original_infobox, original_summary, subject_type)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-        returning patch.id
     """,
         pk,
         subject_id,
