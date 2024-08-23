29eed97fcf11a88491bc9b8373f7eec487931756
Trim21 <trim21.me@gmail.com>
2024-08-23 15:50:31 +0800

    add migration

--- server/migration.py
+++ server/migration.py
@@ -10,7 +10,47 @@ class Migrate(NamedTuple):
     sql: str
 
 
-migrations: list[Migrate] = [Migrate(1, "select 1")]
+migrations: list[Migrate] = [
+    Migrate(1, "select 1"),
+    Migrate(
+        2,
+        """
+create table episode_patch
+(
+    id                   uuid primary key                                   not null,
+    episode_id           integer                                            not null,
+    state                integer                  default 0                 not null,
+    from_user_id         integer                                            not null,
+    wiki_user_id         integer                  default 0                 not null,
+    reason               text                                               not null,
+
+    original_name        text,
+    name                 text,
+
+    original_name_cn     text,
+    name_cn              text,
+
+    original_duration    varchar(255),
+    duration             varchar(255),
+
+    original_airdate     varchar(64),
+    airdate              varchar(64),
+
+    original_description text,
+    description          text,
+
+    created_at           timestamp with time zone default CURRENT_TIMESTAMP not null,
+    updated_at           timestamp with time zone default CURRENT_TIMESTAMP not null,
+    deleted_at           timestamp with time zone,
+
+    reject_reason        varchar(255)             default ''                not null
+);
+
+
+create index on episode_patch (state);
+""",
+    ),
+]
 
 
 async def run_migration() -> None:
