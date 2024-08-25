e96319a965eb87e467913790bd16dfa6d142ecb6
Trim21 <trim21.me@gmail.com>
2024-08-25 09:12:02 +0800

    check more char

--- server/verify.py
+++ server/verify.py
@@ -3,9 +3,13 @@ import unicodedata
 from server.base import BadRequestException
 
 
+_invalid_category = {"Cf", "Mn"}
+
+
 def check_invalid_input_str(*s: str) -> None:
     for ss in s:
         for c in ss:
             v = unicodedata.category(c)
-            if v == "Cf":  # Format
+            print(repr(c), v)
+            if v in _invalid_category:  # Format
                 raise BadRequestException("invalid character {!r}".format(c))
