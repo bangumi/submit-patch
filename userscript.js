9f19e76ee842ad1db21d78faf7be54139378fdfd
Trim21 <trim21.me@gmail.com>
2024-08-27 11:34:43 +0800

    add userscript

---
+++ userscript.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @include       /^https://[^/]*/ep/[^/]*$/
+// @include       /^https://[^/]*/subject/[^/]*/edit$/
+// @include       /^https://[^/]*/subject/[^/]*$/
+// @include       /^https://[^/]*/$/
+// ==/UserScript==
+
+(() => {
+  const path = document.location.pathname
+
+  if (path === '/') {
+    $('h1').append(`&nbsp;<a href="https://patch.bgm38.tv/" target='_blank'><img src="https://patch.bgm38.tv/badge.svg"/></a>`)
+    return;
+  }
+
+  const episodeMatch = /^\/ep\/(\d+)/.exec(path);
+  if (episodeMatch.length) {
+    const episodeID = episodeMatch[1];
+    $('h1.nameSingle').append(`<a href="https://patch.bgm38.tv/" target='_blank'><img src="https://patch.bgm38.tv/badge/subject/${episodeID}"/></a>`);
+
+    $('#columnEpA > h2.title').append(`<small><a href="https://patch.bgm38.tv/suggest-episode?episode_id=${episodeID}" class="l" target="_blank">[提供修改建议]</a></small>`);
+    return
+  }
+
+  const subjectMatch = /^\/subject\/(\d+)/.exec(path);
+  if (subjectMatch.length) {
+    const subjectID = subjectMatch[1];
+    if (/^\/subject\/\d+\/edit$/.test(path)) {
+      $('#columnInSubjectA > a:contains("[修改]")').after(`<a href="https://patch.bgm38.tv/suggest?subject_id=${subjectID}" class="l rr" target="_blank">[提供修改建议]</a>`);
+    } else if (/^\/subject\/\d+$/.test(path)) {
+      $('h1.nameSingle').append(`<a href="https://patch.bgm38.tv/" target='_blank'><img src="https://patch.bgm38.tv/badge/subject/${subjectID}"/></a>`)
+    }
+  }
+})();
