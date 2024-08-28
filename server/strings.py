19536bf9d4d53839f1c6ce9f07bd23d593a8d43d
Trim21 <trim21.me@gmail.com>
2024-08-28 16:37:33 +0800

    fix invalid char

--- server/strings.py
+++ server/strings.py
@@ -9,12 +9,15 @@ _invalid_category = {
     "Cf",
 }
 
+__white_space_char = {"\t", "\r", "\n"}
+
 
 def check_invalid_input_str(*ss: str) -> None:
     for s in ss:
         for c in s:
-            if c == "\t":
+            if c in __white_space_char:
                 continue
+
             v = unicodedata.category(c)
             if v in _invalid_category:
                 raise BadRequestException("invalid character {!r}".format(c))
@@ -24,6 +27,10 @@ def invisible_escape(s: str) -> str:
     with io.StringIO() as f:
         for ss in s:
             for c in ss:
+                if c in __white_space_char:
+                    f.write(c)
+                    continue
+
                 v = unicodedata.category(c)
                 if v in _invalid_category:
                     f.write(c.encode("unicode-escape").decode())
