cbf436ad3e48581ec63eb7a69ad97f562fa25d16
Trim21 <trim21.me@gmail.com>
2024-08-28 16:09:26 +0800

    show escape string

--- server/strings.py
+++ server/strings.py
@@ -1,24 +1,33 @@
-_invalid_category = {"Cf", "Mn", "Mc"}
+import io
+import unicodedata
+
+from server.base import BadRequestException
 
 
-def check_invalid_input_str(*s: str) -> None:
-    return
-    # for ss in s:
-    #     for c in ss:
-    #         v = unicodedata.category(c)
-    #         if v in _invalid_category:
-    #             raise BadRequestException("invalid character {!r}".format(c))
+_invalid_category = {
+    "Cc",
+    "Cf",
+}
+
+
+def check_invalid_input_str(*ss: str) -> None:
+    for s in ss:
+        for c in s:
+            if c == "\t":
+                continue
+            v = unicodedata.category(c)
+            if v in _invalid_category:
+                raise BadRequestException("invalid character {!r}".format(c))
 
 
 def invisible_escape(s: str) -> str:
-    return s
-    # with io.StringIO() as f:
-    #     for ss in s:
-    #         for c in ss:
-    #             v = unicodedata.category(c)
-    #             if v in _invalid_category:
-    #                 f.write(c.encode("unicode-escape").decode())
-    #             else:
-    #                 f.write(c)
-    #
-    #     return f.getvalue()
+    with io.StringIO() as f:
+        for ss in s:
+            for c in ss:
+                v = unicodedata.category(c)
+                if v in _invalid_category:
+                    f.write(c.encode("unicode-escape").decode())
+                else:
+                    f.write(c)
+
+        return f.getvalue()
