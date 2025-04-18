336311418968349912fb250fb235fc747205caff
Trim21 <trim21.me@gmail.com>
2025-04-19 04:10:12 +0800

    wip

--- templates.go
+++ templates.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/Masterminds/sprig/v3"
 	"github.com/valyala/bytebufferpool"
+
+	"app/view"
 )
 
 const baseTemplates = "view/templates/base/*.gohtml"
@@ -16,9 +18,9 @@ const baseTemplates = "view/templates/base/*.gohtml"
 func loadTemplates(config Config) (Template, error) {
 	if config.Debug {
 		return Template{
-			NewSubjectPatch: &devPage{name: "subject-edit.gohtml"},
-			Debug:           &devPage{name: "debug.gohtml"},
-			Login:           &devPage{name: "login.gohtml"},
+			NewSubjectPatch: &devPage[view.SubjectPatchEdit]{name: "subject-edit.gohtml"},
+			Debug:           &devPage[any]{name: "debug.gohtml"},
+			Login:           &devPage[any]{name: "login.gohtml"},
 		}, nil
 	}
 
@@ -28,39 +30,39 @@ func loadTemplates(config Config) (Template, error) {
 	}).ParseFS(templateFiles, baseTemplates))
 
 	return Template{
-		NewSubjectPatch: loadProdPage(base, "subject-edit.gohtml"),
-		Debug:           loadProdPage(base, "debug.gohtml"),
-		Login:           loadProdPage(base, "login.gohtml"),
+		NewSubjectPatch: loadProdPage[view.SubjectPatchEdit](base, "subject-edit.gohtml"),
+		Debug:           loadProdPage[any](base, "debug.gohtml"),
+		Login:           loadProdPage[any](base, "login.gohtml"),
 	}, nil
 }
 
-func loadProdPage(base *template.Template, name string) *prodPage {
+func loadProdPage[T any](base *template.Template, name string) *prodPage[T] {
 	raw, err := fs.ReadFile(templateFiles, "view/templates/page/"+name)
 	if err != nil {
 		panic(err)
 	}
 
-	return &prodPage{
+	return &prodPage[T]{
 		tmpl: template.Must(template.Must(base.Clone()).Parse(string(raw))),
 	}
 }
 
 type Template struct {
-	NewSubjectPatch Page
-	Debug           Page
-	Login           Page
+	NewSubjectPatch Page[view.SubjectPatchEdit]
+	Debug           Page[any]
+	Login           Page[any]
 }
 
-type Page interface {
-	Execute(wr io.Writer, data any) error
+type Page[T any] interface {
+	Execute(wr io.Writer, data T) error
 }
 
-type prodPage struct {
+type prodPage[T any] struct {
 	pool bytebufferpool.Pool
 	tmpl *template.Template
 }
 
-func (p *prodPage) Execute(wr io.Writer, data any) error {
+func (p *prodPage[T]) Execute(wr io.Writer, data T) error {
 	buf := p.pool.Get()
 	defer p.pool.Put(buf)
 
@@ -73,11 +75,11 @@ func (p *prodPage) Execute(wr io.Writer, data any) error {
 	return nil
 }
 
-type devPage struct {
+type devPage[T any] struct {
 	name string
 }
 
-func (p *devPage) Execute(wr io.Writer, data any) error {
+func (p *devPage[T]) Execute(wr io.Writer, data T) error {
 	t := template.Must(template.New("").Funcs(sprig.FuncMap()).Funcs(template.FuncMap{
 		"setQuery": setQuery,
 		"seq64":    seq64,
