732887ce4696f1ab6685bf7a6e546e46bc9de656
Trim21 <trim21.me@gmail.com>
2025-04-21 20:16:16 +0800

    fix: comments padding

--- templates/episode_templ.go
+++ templates/episode_templ.go
@@ -192,7 +192,7 @@ func episodePatchDetailContent(
 			return templ_7745c5c3_Err
 		}
 		if len(comments) > 0 {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 12, "<div class=\"row mb-2\"><ul class=\"list-group\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 12, "<div class=\"row mb-2\"><ul class=\"list-group ps-2 pe-2\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
--- templates/subject_templ.go
+++ templates/subject_templ.go
@@ -221,7 +221,7 @@ func subjectPatchDetailContent(
 			return templ_7745c5c3_Err
 		}
 		if len(comments) > 0 {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 13, "<div class=\"row mb-2\"><ul class=\"list-group\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 13, "<div class=\"row mb-2\"><ul class=\"list-group ps-2 pe-2\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
