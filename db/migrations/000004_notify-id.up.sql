8f5d545bbe615acac7fdd0fd993d9b22fcfd7eb4
Trim21 <trim21.me@gmail.com>
2025-09-18 21:24:39 +0800

    fix

--- db/migrations/000004_notify-id.up.sql
+++
@@ -1,5 +0,0 @@
-alter table subject_patch add column num_id bigserial not null;
-alter table episode_patch add column num_id bigserial not null;
-
-create index idx_subject_patch_num_id on subject_patch (num_id);
-create index idx_episode_patch_num_id on episode_patch (num_id);
